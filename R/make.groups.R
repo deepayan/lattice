1100625d1e851f84c1971404be37f0ba43568dc3
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2005-07-15 23:20:23 +0000

    added 'make.groups' (no doc yet), minor updates to several doc pages

---
+++ R/make.groups.R
@@ -0,0 +1,40 @@
+
+
+### Copyright (C) 2005 Deepayan Sarkar <Deepayan.Sarkar@R-project.org> and 
+###
+### This file is part of the lattice library for R.
+### It is made available under the terms of the GNU General Public
+### License, version 2, or at your option, any later version,
+### incorporated herein by reference.
+###
+### This program is distributed in the hope that it will be
+### useful, but WITHOUT ANY WARRANTY; without even the implied
+### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+### PURPOSE.  See the GNU General Public License for more
+### details.
+###
+### You should have received a copy of the GNU General Public
+### License along with this program; if not, write to the Free
+### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+### MA 02111-1307, USA
+
+
+
+make.groups <- function(...)
+{
+    tmp <- list(...)
+    nms <- as.character(substitute(list(...)))[-1]
+    if (is.null(names(tmp)))
+        names(tmp) <- nms
+    else
+    {
+        unnamed <- names(tmp) == ""
+        names(tmp)[unnamed] <- nms[unnamed]
+    }
+    data.frame(data = unlist(tmp),
+               which =
+               rep(gl(length(tmp), 1, labels = names(tmp)),
+                   sapply(tmp, length)))
+}
+
+
