b6fc0dc4d6549ef1f60d921c4d2e9f022c497d82
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2005-12-22 23:03:50 +0000

    updated translations and log

--- update.sh
+++ update.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env sh
 
-
 ## Usage: ./update.sh 
-##
-
-## Tasks: (1) update po files (2) update SvnLog
+## 
+## has to be run in current directory
+## 
+## Tasks: 
+##   (1) update translation templates 
+##   (2) run msgmerge on existing translation catalogs
+##   (2) update SvnLog [actually create a file with latest entries]
 
 R_PROG=R-2.2
 echo "library(tools); xgettext2pot('.', 'po/R-lattice.pot')" | ${R_PROG} --vanilla --silent
+for f in po/R-*.po; do msgmerge --no-wrap --update $f po/R-lattice.pot; done
 
+## Note: -o is relatively new in grep
 LASTLOG=`grep -o -m 1 "r[0-9]* |" SvnLog | sed -e 's/[^0-9]//g'`
-
 svn log -v -r HEAD:${LASTLOG} > SvnLog.update
-
-
-
-
-
-
 
