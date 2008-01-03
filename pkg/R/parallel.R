d2e46aeb96e9a508bfca6ca7f6528dfbe1ca10cd
deepayan <deepayan@3a60a7e4-63ee-4330-9c61-e6b197b972ce>
2008-01-03 18:13:56 +0000

    better scoping behaviour

--- pkg/R/parallel.R
+++ pkg/R/parallel.R
@@ -197,15 +197,20 @@ parallel <- function(x, data, ...) UseMethod("parallel")
 ##     else UseMethod("parallel")
 ## }
 
+
+
 parallel.matrix <-
 parallel.data.frame <-
-    function(x, data = NULL, ...)
+    function(x, data = NULL, ..., groups = NULL, subscripts = TRUE)
 {
     ccall <- match.call()
     if (!is.null(ccall$data)) 
         warning("explicit 'data' specification ignored")
-    ccall$data <- list(x = x)
+    ccall$data <-
+        list(x = x, groups = groups, subscripts = subscripts)
     ccall$x <- ~x
+    ccall$groups <- groups
+    ccall$subscripts <- subscripts
     ccall[[1]] <- quote(lattice::parallel)
     eval.parent(ccall)
 }
