e90f121a633340faaf1f2ef98c67cacc0da843cd
deepayan <deepayan@3a60a7e4-63ee-4330-9c61-e6b197b972ce>
2012-09-13 09:46:23 +0000

    panel.levelplot bugfix for length-1 at

--- pkg/R/levelplot.R
+++ pkg/R/levelplot.R
@@ -144,7 +144,9 @@ panel.levelplot <-
 ##             col.regions[round(seq(1, numcol.r, length.out = numcol))]
 ##     zcol <- cut(z, at, include.lowest = TRUE, labels = FALSE)
 
-    zcol <- level.colors(z, at, col.regions, colors = TRUE)
+    zcol <-
+        if (region) level.colors(z, at, col.regions, colors = TRUE)
+        else "transparent"
 
     x <- x[subscripts]
     y <- y[subscripts]
@@ -152,7 +154,7 @@ panel.levelplot <-
     minYwid <- if (length(unique(y)) > 1) min(diff(sort(unique(y)))) else 1
     fullZrange <- range(as.numeric(z), finite = TRUE) # for shrinking
     z <- z[subscripts]
-    zcol <- zcol[subscripts]
+    if (region) zcol <- zcol[subscripts]
 
     if (hasArg(group.number))
         group <- list(...)$group.number
